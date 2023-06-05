fcd807ac88492337c93b7af41f4d30dd89e86554
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-06-05 19:38:24 +0300

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Beste Kocaoglu on 5.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
